19db8a565886d6b0c849ce3e6d12c5ff5a02b3a9
R4DYY <radiswarrior@gmail.com>
2026-09-09 20:48:04 +0200

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { tikTokBridgePlugin } from './server/live-bridge/vitePlugin';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tikTokBridgePlugin()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
